cdcde30ca92d3c0c21dc798adc59c99eb315ff2a
dokkodo <callum.dokkodo@gmail.com>
2025-03-07 16:39:59 +0200

    progress

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
-import sys
 import json
 import random
 
-
 def main():
-    print(select_country())
-#   choose_dish(select_country)
-
+    selected_country = select_country()
+    choose_dish(selected_country)
 
 def select_country():
     yes = ["y", "yes", "yup", "yip"]
     no = ["n", "no", "nope"]
+
     with open("data/countries.json", "r") as f:
         data = json.load(f)
 
     while True:
-        selected_country = random.choice(data["countries"])
-        print(f"{selected_country}\n{selected_country.description}")
-        user_response = input("Would you like to choose a different country?\n\"Yes\", \"No\"\n")
+        selected_country = random.choice(list(data["countries"].keys()))
+        country_data = data["countries"][selected_country]
+
+        print(f"\n{selected_country}\n{country_data['description']}")
+
+        user_response = input("Would you like to choose a different country? (Yes/No)\n").strip().lower()
+        
         if user_response in yes:
             continue
         elif user_response in no:
-            print(f"{selected_country.languages}\n{selected_country.greeting}")
+            print(f"\n{country_data['languages']}\n{country_data['greeting']}")
             return selected_country
         else:
-            print("Invalid response")
+            print("Invalid response.")
             continue
-
 
 def choose_dish(country):
     with open("data/dishes.json", "r") as f:
-        data = json.load(f)
-    dishes = data[{country}]
+        dish_data = json.loads(f)
+    with open("data/countries.json", "r") as f:
+        country_data = json.loads(f)
+        
+    dishes = country_data[country]["dishes"]
 
     for dish in dishes:
-        print(f"{dish.name}\n{dish.description}")
+        print(f"{dish.name}\n{dish.description}") # replace
     while True:
         selected_dish = input("Which dish would you like to see?\n")
         if selected_dish not in dishes:
             print("Invalid dish")
             continue
-        print(f"{selected_dish.recipe}\n{selected_dish.steps}")
+        print(f"{selected_dish.recipe}\n{selected_dish.steps}") # replace
         break
 
-    
 if __name__ == "__main__":
     main()
